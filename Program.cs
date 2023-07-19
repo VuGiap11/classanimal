5b95e87c2700287a45b7e9146a1de0fa5e8e9439
vu giap <vudinhgiap11@gmail.com>
2023-07-19 22:48:18 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classanimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat1 = new Cat("1m47", "50kg", "****");
+            cat1.Showinfo();
+        }
+    }
+}
---
+++ animal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classanimal
+{
+    internal abstract class Animal
+    {
+        private string height;
+        private string weight;
+
+        public string Height { get => height; set => height = value; }
+        public string Weight { get => weight; set => weight = value; }
+
+
+        public Animal(string height, string weight)
+        {
+            this.Height = height;
+            this.Weight = weight;
+        }
+
+        public abstract void Showinfo();
+    }
+
+    class Cat : Animal
+    {
+        private string name;
+        public string Name { get => name; set => name = value; }
+
+        public Cat(string height, string weight, string name) : base(height, weight)
+        {
+            this.Name = name;
+        }
+        public override void Showinfo()
+        {
+            Console.WriteLine("height: {0} " + '\n' + "weight : {1}" + '\n' + "name: {2}", this.Height, this.Weight, this.Name);
+        }
+    }
+}
